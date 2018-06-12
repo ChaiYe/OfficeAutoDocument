bb3bed8b7855b29382ab9bc573a3f583779ee536
AchmGitHub <670509577@qq.com>
2018-06-12 21:33:07 +0800

    new sql
    里程碑表，投票表

--- officeauto.sql
+++ officeauto.sql
@@ -24,8 +24,8 @@ DROP TABLE IF EXISTS `actfile`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `actfile` (
   `uuid` int(11) NOT NULL AUTO_INCREMENT,
-  `path` varchar(1000) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `path` varchar(1000) NOT NULL COMMENT '文件的绝对路径',
+  `name` varchar(255) NOT NULL COMMENT '文件原来的名字',
   `format` varchar(100) NOT NULL,
   `size` int(11) NOT NULL,
   `createtime` date NOT NULL,
@@ -36,7 +36,7 @@ CREATE TABLE `actfile` (
   KEY `actfile_employee_idx` (`employee`),
   CONSTRAINT `actfile_activity` FOREIGN KEY (`activity`) REFERENCES `activity` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `actfile_employee` FOREIGN KEY (`employee`) REFERENCES `employee` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -45,7 +45,7 @@ CREATE TABLE `actfile` (
 
 LOCK TABLES `actfile` WRITE;
 /*!40000 ALTER TABLE `actfile` DISABLE KEYS */;
-INSERT INTO `actfile` VALUES (1,'H:\\OAstorage\\test.txt','test','txt',24,'2018-12-21',1,1),(2,'H:\\OAstorage\\test.txt','test','txt',24,'2018-12-21',1,1),(3,'H:\\OAstorage\\test.txt','test','txt',24,'2018-12-21',1,1),(4,'H:\\OAstorage\\test.txt','test','txt',24,'2018-12-21',1,1),(5,'H:\\OAstorage\\test.txt','test','txt',24,'2018-12-21',1,1),(6,'H:\\OAstorage\\9e11b617-6feb-49e9-8b7d-8fff8702db20.jpg','1512794110649.jpg','jpg',2485399,'2018-05-25',1,1),(7,'D:\\OAstorage\\591cd7f8-aabd-46ba-8b49-630ef23fc238.xls','第8组项目2进度表.xls','xls',29184,'2018-05-28',1,1);
+INSERT INTO `actfile` VALUES (8,'D:\\OAstorage\\1\\883e161e-4dec-4225-8c6b-ee3d06e7c8f6.txt','测试文件.txt','.txt',2334,'2018-06-10',1,1),(9,'D:\\OAstorage\\1\\5cd1a2e2-4d37-413d-a9c7-90085590a9ee.xls','第8组项目2进度表.xls','.xls',29184,'2018-06-10',1,1),(10,'D:\\OAstorage\\1\\9653c49e-27ec-4b81-8002-12a6912b12b9.vsdx','办公自动化系统.vsdx','.vsdx',351386,'2018-06-10',1,1),(11,'D:\\OAstorage\\1\\3957cccf-e6ba-4d98-bafd-c26fa3fcc316.doc','办公自动化需求分析说明书 .doc','.doc',1135104,'2018-06-10',1,1),(12,'D:\\OAstorage\\1\\494bf1de-90d5-429c-b584-ae8c21659ad3.ppt','第7章  网络办公自动化系统的研究与.ppt','.ppt',1581568,'2018-06-10',1,1),(13,'D:\\OAstorage\\1\\6d0d3eba-70d5-4497-af66-e3e745bd0a34.txt','测试文件.txt','.txt',2334,'2018-06-10',1,1),(14,'D:\\OAstorage\\1\\b0253306-f79c-4aa9-950d-c7720768ddc3.txt','测试文件.txt','.txt',2334,'2018-06-10',1,1),(15,'D:\\OAstorage\\1\\2c3b88a9-f831-4ec3-90b7-b17832c2fee9.txt','测试文件.txt','.txt',2334,'2018-06-10',1,1),(16,'D:\\OAstorage\\1\\3ce07391-cac3-4dab-806f-e6f4d0dff07a.txt','测试文件.txt','.txt',2334,'2018-06-10',1,1),(17,'D:\\OAstorage\\1\\a6d2c817-822f-4337-abe6-a55e2dc03cff.jpg','14ab8759-d897-445d-8156-ea9fe1ca20cd.jpg','.jpg',269915,'2018-06-10',1,1);
 /*!40000 ALTER TABLE `actfile` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -62,7 +62,7 @@ CREATE TABLE `activity` (
   `descript` text NOT NULL,
   `begintime` date NOT NULL,
   `endtime` date NOT NULL,
-  `state` int(11) NOT NULL COMMENT '状态， 待审批，正在进行，已结束',
+  `state` int(11) NOT NULL COMMENT '状态， 待审批 0，未开始 1，正在进行 2，已结束 3',
   `dept` int(11) NOT NULL,
   `employee` int(11) NOT NULL,
   PRIMARY KEY (`uuid`),
@@ -70,7 +70,7 @@ CREATE TABLE `activity` (
   KEY `activity_dept_idx` (`dept`),
   CONSTRAINT `activity_dept` FOREIGN KEY (`dept`) REFERENCES `dept` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `activity_employee` FOREIGN KEY (`employee`) REFERENCES `employee` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -79,8 +79,36 @@ CREATE TABLE `activity` (
 
 LOCK TABLES `activity` WRITE;
 /*!40000 ALTER TABLE `activity` DISABLE KEYS */;
-INSERT INTO `activity` VALUES (1,'年庆','开趴体','2018-05-12','2018-05-12',1,1,1),(2,'中秋晚会','仙庙烧鸡聚会','2018-08-15','2018-08-16',2,1,1),(4,'测试','测试','2018-08-15','2018-08-15',1,1,1),(5,'测试','测试','2018-08-15','2018-08-15',1,1,1);
+INSERT INTO `activity` VALUES (1,'年庆','开趴体<p><b>测试这是一个测试，测试添加， 测试主页数据</b></p><p><b><i>测试这是一个测试，测试添加， 测试主页数据</i></b></p><p><b>测试这是一个测试，测试添加， 测试主页数据</b></p>','2018-06-08','2018-12-12',2,1,1),(2,'中秋晚会','仙庙烧鸡聚会','2018-08-15','2018-08-16',1,1,1),(4,'测试','测试','2018-08-15','2018-08-15',1,1,1),(5,'测试','测试','2018-08-15','2018-08-15',1,1,1),(6,'测试测试测试测试','<p>测试测试测试测试测试测试测试测试测试测试</p>','2018-06-02','2018-06-05',3,1,1),(7,'测试这是一个测试','<p><b>测试这是一个测试，测试添加， 测试主页数据</b></p><p><b><i>测试这是一个测试，测试添加， 测试主页数据</i></b></p><p><b>测试这是一个测试，测试添加， 测试主页数据</b></p>','2018-06-09','2018-06-30',2,2,1);
 /*!40000 ALTER TABLE `activity` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `actmarker`
+--
+
+DROP TABLE IF EXISTS `actmarker`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `actmarker` (
+  `uuid` int(11) NOT NULL AUTO_INCREMENT,
+  `descript` text NOT NULL,
+  `createtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `activity` int(11) NOT NULL,
+  PRIMARY KEY (`uuid`),
+  KEY `marker_activity_idx` (`activity`),
+  CONSTRAINT `marker_activity` FOREIGN KEY (`activity`) REFERENCES `activity` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `actmarker`
+--
+
+LOCK TABLES `actmarker` WRITE;
+/*!40000 ALTER TABLE `actmarker` DISABLE KEYS */;
+INSERT INTO `actmarker` VALUES (1,'第一阶段完成','2018-06-08 03:11:11',1),(2,'第二阶段完成','2018-06-08 04:12:31',1),(3,'第三阶段完成','2018-06-08 10:19:31',1),(4,'第四阶段完成','2018-06-09 00:19:31',1),(5,'第五阶段完成','2018-06-09 03:10:39',1),(6,'第六阶段完成','2018-06-10 03:05:47',1),(7,'第七阶段完成','2018-06-10 03:47:25',1),(8,'第八阶段完成','2018-06-10 03:47:48',1);
+/*!40000 ALTER TABLE `actmarker` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -94,11 +122,11 @@ CREATE TABLE `actoption` (
   `uuid` int(11) NOT NULL AUTO_INCREMENT,
   `descript` varchar(255) NOT NULL,
   `num` int(11) NOT NULL DEFAULT '0',
-  `activity` int(11) NOT NULL,
+  `vote` int(11) NOT NULL,
   PRIMARY KEY (`uuid`),
-  KEY `option_activity_idx` (`activity`),
-  CONSTRAINT `option_activity` FOREIGN KEY (`activity`) REFERENCES `activity` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+  KEY `option_vote_idx` (`vote`),
+  CONSTRAINT `option_vote` FOREIGN KEY (`vote`) REFERENCES `vote` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -107,7 +135,7 @@ CREATE TABLE `actoption` (
 
 LOCK TABLES `actoption` WRITE;
 /*!40000 ALTER TABLE `actoption` DISABLE KEYS */;
-INSERT INTO `actoption` VALUES (1,'活动选项1',2,1),(2,'活动选项2',2,1),(4,'测试',2,1),(5,'测试',2,1);
+INSERT INTO `actoption` VALUES (6,'叫外卖',0,1),(7,'麦当劳',0,1),(8,'随便吃',0,1),(9,'瞎吃',0,1);
 /*!40000 ALTER TABLE `actoption` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,7 +156,7 @@ CREATE TABLE `actvote` (
   KEY `actvote_employee_idx` (`employee`),
   CONSTRAINT `actvote_actoption` FOREIGN KEY (`actoption`) REFERENCES `actoption` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `actvote_employee` FOREIGN KEY (`employee`) REFERENCES `employee` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -137,7 +165,6 @@ CREATE TABLE `actvote` (
 
 LOCK TABLES `actvote` WRITE;
 /*!40000 ALTER TABLE `actvote` DISABLE KEYS */;
-INSERT INTO `actvote` VALUES (1,'2018-05-12',1,1),(2,'2018-12-12',1,2);
 /*!40000 ALTER TABLE `actvote` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -287,7 +314,7 @@ CREATE TABLE `job` (
 
 LOCK TABLES `job` WRITE;
 /*!40000 ALTER TABLE `job` DISABLE KEYS */;
-INSERT INTO `job` VALUES (1,'经理','经理',2,'2015-01-01',1,1),(2,'总监','总监',2,'2015-01-02',1,2),(3,'总裁','总裁',3,'2015-01-01',2,1),(5,'测试','测试',3,'2015-01-01',3,1),(6,'测试','测试',3,'2015-01-01',1,1),(7,'测试','测试',3,'2015-01-01',1,1),(10,'新职位','描述',1,'2018-05-11',1,1);
+INSERT INTO `job` VALUES (1,'经理','经理',2,'2015-01-01',1,1),(2,'总监','总监',1,'2015-01-02',1,2),(3,'总裁','总裁',3,'2015-01-01',2,1),(5,'测试','测试',3,'2015-01-01',3,1),(6,'测试','测试',3,'2015-01-01',1,1),(7,'测试','测试',3,'2015-01-01',1,1),(10,'新职位','描述',1,'2018-05-11',1,1);
 /*!40000 ALTER TABLE `job` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -357,12 +384,15 @@ CREATE TABLE `vote` (
   `uuid` int(11) NOT NULL AUTO_INCREMENT,
   `descript` text NOT NULL,
   `begintime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `endtime` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `endtime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `activity` int(11) NOT NULL,
+  `employee` int(11) NOT NULL,
   PRIMARY KEY (`uuid`),
   KEY `vote_act_idx` (`activity`),
-  CONSTRAINT `vote_act` FOREIGN KEY (`activity`) REFERENCES `activity` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  KEY `vote_employee_idx` (`employee`),
+  CONSTRAINT `vote_act` FOREIGN KEY (`activity`) REFERENCES `activity` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `vote_employee` FOREIGN KEY (`employee`) REFERENCES `employee` (`uuid`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -371,6 +401,7 @@ CREATE TABLE `vote` (
 
 LOCK TABLES `vote` WRITE;
 /*!40000 ALTER TABLE `vote` DISABLE KEYS */;
+INSERT INTO `vote` VALUES (1,'晚上去哪吃','2018-06-08 03:11:11','2018-06-08 14:22:22',1,1);
 /*!40000 ALTER TABLE `vote` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -383,4 +414,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-06-07 20:30:03
+-- Dump completed on 2018-06-12 21:31:39
